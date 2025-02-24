968ebe8d53265d69135474d589d47df73553e21d
Phi Le <philt.tomotek.com>
2025-02-24 16:47:42 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function (req, res) {
+    res.send("Hello World!");
+});
+
+
+app.listen(5005, function () {
+    console.log("Started application on port %d", 5005);
+});
