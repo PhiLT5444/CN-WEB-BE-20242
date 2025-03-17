bf0dff03f30437df2e5b27b6356901c401a4acb7
phongbeovaio <lenamphongsoict@gmail.com>
2025-03-18 00:30:51 +0700

    Thêm logic CRUD

---
+++ src/controllers/userController.js
@@ -0,0 +1,58 @@
+// CRUD
+// src/controllers/userController.js
+const User = require('../models/user');
+
+exports.createUser = async (req, res) => {
+  try {
+    const { username, email, password, role } = req.body;
+    const user = await User.create({ username, email, password, role });
+    res.status(201).json({ message: 'User created', user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { username, email, password, role } = req.body;
+    const user = await User.findByPk(id);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    await user.update({ username, email, password, role });
+    res.status(200).json({ message: 'User updated', user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findByPk(id);
+    if (!user) return res.status(404).json({ message: 'User not found' });
+    await user.destroy();
+    res.status(200).json({ message: 'User deleted' });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
