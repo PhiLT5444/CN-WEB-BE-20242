0dede560bac2c7044d854986263d798aff07b356
phongbeovaio <lenamphongsoict@gmail.com>
2025-03-18 00:32:05 +0700

    Update app.js để bao gồm userRouter

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 // src/app.js
 const express = require('express');
 const sequelize = require('./config/database');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
 // Middleware để parse JSON
 app.use(express.json());
+
+// Sử dụng routes
+app.use('/api', userRoutes);
 
 // Đồng bộ database
 sequelize.sync({ force: false }).then(() => {
