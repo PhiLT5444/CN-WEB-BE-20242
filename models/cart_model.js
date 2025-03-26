1883d679c9ac3b443b67f39da64534d05941e956
Đỗ Kim Nhường <127850790+KimNhuong@users.noreply.github.com>
2025-03-26 15:54:30 +0700

    chỉnh lại Cart model

--- models/cart_model.js
+++ models/cart_model.js
@@ -1,26 +1,27 @@
-module.exports = (sequelize, DataTypes) => {
-  const Cart = sequelize.define(
-    "Cart",
-    {
-      user_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      product_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      quantity: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: 1,
-      },
-    },
-    {
-      tableName: "carts",
-      timestamps: true,
-    }
-  );
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
 
-  return Cart;
-};
+const Cart = sequelize.define(
+  "Cart",
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1,
+    },
+  },
+  {
+    tableName: "carts",
+    timestamps: true,
+  }
+);
+
+module.exports = Cart;
