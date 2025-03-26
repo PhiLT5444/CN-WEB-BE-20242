c0eecc8a916e9b3d687e223133a2c3bf145171d2
Đỗ Kim Nhường <127850790+KimNhuong@users.noreply.github.com>
2025-03-26 16:16:59 +0700

    Thêm database.js, chỉnh lại cart model

--- models/cart_model.js
+++ models/cart_model.js
@@ -7,21 +7,33 @@ const Cart = sequelize.define(
     user_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      primaryKey: true, // Khóa chính
     },
     product_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      primaryKey: true, // Khóa chính
     },
     quantity: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      defaultValue: 1,
+    },
+    is_deleted: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
   },
   {
     tableName: "carts",
-    timestamps: true,
+    timestamps: false,
   }
 );
+
+// Thiết lập quan hệ với User & Product
+const User = require("./User");
+const Product = require("./Product");
+
+Cart.belongsTo(User, { foreignKey: "user_id", as: "user" });
+Cart.belongsTo(Product, { foreignKey: "product_id", as: "product" });
 
 module.exports = Cart;
