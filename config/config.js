80978ec332b83a3a0cb53d68b4cc160e33fe9b45
Phi Le <philt.tomotek.com>
2025-04-17 17:15:37 +0700

    update config

--- config/config.js
+++ config/config.js
@@ -6,6 +6,6 @@ module.exports = {
     password: process.env.DB_PASSWORD || 'admin',
     database: process.env.DB_NAME || 'product_store',
     host: process.env.DB_HOST || '127.0.0.1',
-    dialect: 'mysql'
+    dialect: 'mysql',
   },
 };
