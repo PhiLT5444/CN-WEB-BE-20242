e6f742cef213171149e6019875b74cc1fd71d9c9
TuanNinhBK67 <tuanninhhust2022@gmail.com>
2025-04-25 21:14:11 +0700

    modify the config

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
-  database: 'commercial_store',
+  database: process.env.DB_NAME || 'product_store',
   username: process.env.MYSQL_USERNAME || 'root',
-  password: null,
+  password: 'admin',
   host: process.env.MYSQL_HOST || '127.0.0.1',
   dialect: 'mysql',
   port: process.env.MYSQL_PORT || 3306,
